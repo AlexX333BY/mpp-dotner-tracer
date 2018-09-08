b2b5a236e96da0f8e173b9749299323709ad7c12
Aliaksei <alexx333by@gmail.com>
2018-09-08 15:04:29 +0300

    Created Tracer skeleton

--- Tracer/Tracer/Tracer.cs
+++ Tracer/Tracer/Tracer.cs
@@ -4,19 +4,47 @@ namespace Tracer
 {
     public class Tracer : ITracer
     {
+        private bool isWorking;
+        public bool IsWorking
+        {
+            get => isWorking;
+            protected set => isWorking = value;
+        }
+
         public TraceResult GetTraceResult()
         {
-            throw new NotImplementedException();
+            if (IsWorking)
+            {
+                throw new TracerIsBusyException();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public void StartTrace()
         {
-            throw new NotImplementedException();
+            if (IsWorking)
+            {
+                throw new TracerIsBusyException();
+            }
+            else
+            {
+                IsWorking = true;
+                throw new NotImplementedException();
+            }
         }
 
         public void StopTrace()
         {
+            IsWorking = false;
             throw new NotImplementedException();
+        }
+
+        public Tracer()
+        {
+            isWorking = false;
         }
     }
 }
