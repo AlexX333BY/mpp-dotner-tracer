e336522495045f449b4f9363b632684f2cc92903
Aliaksei <alexx333by@gmail.com>
2018-09-09 22:42:29 +0300

    XmlTraceResultSerializer implemented

---
+++ Tracer/Tracer/XmlTraceResultSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Xml;
+using System.Runtime.Serialization;
+
+namespace Tracer
+{
+    public class XmlTraceResultSerializer : ITraceResultSerializer
+    {
+        public Stream Stream { protected get; set; }
+        protected readonly XmlWriterSettings xmlWriterSettings;
+        protected readonly DataContractSerializer xmlSerializer;
+
+        public void SerializeTraceResult(TraceResult traceResult)
+        {
+            using (XmlWriter xmlWriter = XmlWriter.Create(Stream, xmlWriterSettings))
+            {
+                xmlSerializer.WriteObject(xmlWriter, traceResult);
+            }
+        }
+
+        public XmlTraceResultSerializer()
+        {
+            Stream = null;
+            xmlWriterSettings = new XmlWriterSettings();
+            xmlWriterSettings.Indent = true;
+            xmlSerializer = new DataContractSerializer(typeof(TraceResult));
+        }
+
+        public XmlTraceResultSerializer(Stream stream)
+            : this()
+        {
+            Stream = stream;
+        }
+    }
+}
