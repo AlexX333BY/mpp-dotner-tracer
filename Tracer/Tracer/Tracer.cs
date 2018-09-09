dc6aa5654a708de155ce0ac1a72838ef734118ec
Aliaksei <alexx333by@gmail.com>
2018-09-09 16:56:54 +0300

    Removed wrong Tracer skelekton

--- Tracer/Tracer/Tracer.cs
+++ Tracer/Tracer/Tracer.cs
@@ -4,48 +4,19 @@ namespace Tracer
 {
     public class Tracer : ITracer
     {
-        protected bool isWorking;
-
-        public bool IsWorking
-        {
-            get => isWorking;
-            protected set => isWorking = value;
-        }
-
         public TraceResult GetTraceResult()
         {
-            if (IsWorking)
-            {
-                throw new TracerIsBusyException();
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
 
         public void StartTrace()
         {
-            if (IsWorking)
-            {
-                throw new TracerIsBusyException();
-            }
-            else
-            {
-                IsWorking = true;
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
 
         public void StopTrace()
         {
-            IsWorking = false;
             throw new NotImplementedException();
-        }
-
-        public Tracer()
-        {
-            isWorking = false;
         }
     }
 }
