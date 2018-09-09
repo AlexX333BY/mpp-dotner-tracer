1b333fc6d13986c75764fecd1d3144506e710fca
Aliaksei <alexx333by@gmail.com>
2018-09-09 22:50:58 +0300

    JsonTraceResultSerializer: added json settings as field and using statement in SerializeTraceResult method

--- Tracer/Tracer/JsonTraceResultSerializer.cs
+++ Tracer/Tracer/JsonTraceResultSerializer.cs
@@ -8,20 +8,24 @@ namespace Tracer
     public class JsonTraceResultSerializer : ITraceResultSerializer
     {
         public Stream Stream { protected get; set; }
+        protected readonly DataContractJsonSerializer jsonSerializer;
 
         public void SerializeTraceResult(TraceResult traceResult)
         {
-            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(TraceResult));
-            XmlDictionaryWriter jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(Stream, Encoding.UTF8, true, true);
-            jsonSerializer.WriteObject(jsonWriter, traceResult);
+            using (XmlDictionaryWriter jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(Stream, Encoding.UTF8, true, true))
+            {
+                jsonSerializer.WriteObject(jsonWriter, traceResult);
+            }
         }
 
         public JsonTraceResultSerializer()
         {
             Stream = null;
+            jsonSerializer = new DataContractJsonSerializer(typeof(TraceResult));
         }
 
         public JsonTraceResultSerializer(Stream stream)
+            : this()
         {
             Stream = stream;
         }
