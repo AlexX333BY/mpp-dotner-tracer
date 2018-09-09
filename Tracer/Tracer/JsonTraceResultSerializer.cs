e5cf7a83068acc63078dbf8393053ce712bdef16
Aliaksei <alexx333by@gmail.com>
2018-09-09 19:56:07 +0300

    Created JSON serializer class

---
+++ Tracer/Tracer/JsonTraceResultSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    class JsonTraceResultSerializer : ITraceResultSerializer
+    {
+        protected Stream stream;
+
+        public void SerializeTraceResult(TraceResult traceResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStream(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonTraceResultSerializer()
+        {
+            stream = null;
+        }
+
+        public JsonTraceResultSerializer(Stream stream)
+        {
+            this.stream = stream;
+        }
+    }
+}
