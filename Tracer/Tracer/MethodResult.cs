9f854a6e92f6c2884a27aaf6709d75ff27312297
Aliaksei <alexx333by@gmail.com>
2018-09-12 00:54:51 +0300

    *Result classes placed in separate files

---
+++ Tracer/Tracer/MethodResult.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace Tracer
+{
+    [DataContract(Name = "method")]
+    public class MethodResult
+    {
+        private List<MethodResult> innerMethods;
+        private Stopwatch stopWatch;
+
+        [DataMember(Name = "name")]
+        public string MethodName
+        { get; internal set; }
+
+        [DataMember(Name = "class")]
+        public string ClassName
+        { get; internal set; }
+
+        public long Time
+        {
+            get => stopWatch.ElapsedMilliseconds;
+        }
+
+        [DataMember(Name = "time")]
+        public string TimeWithPostfix
+        {
+            get => Time.ToString() + "ms";
+            private set { } // to allow serialization
+        }
+
+        [DataMember(Name = "methods")]
+        public List<MethodResult> InnerMethods
+        {
+            get => new List<MethodResult>(innerMethods);
+            private set { } // to allow serialization
+        }
+
+        internal void StartTrace()
+        {
+            stopWatch.Start();
+        }
+
+        internal void StopTrace()
+        {
+            stopWatch.Stop();
+        }
+
+        internal void AddInnerMethod(MethodResult methodResult)
+        {
+            innerMethods.Add(methodResult);
+        }
+
+        internal MethodResult()
+        {
+            innerMethods = new List<MethodResult>();
+            stopWatch = new Stopwatch();
+        }
+    }
+}
---
+++ Tracer/Tracer/ThreadResult.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Tracer
+{
+    [DataContract(Name = "thread")]
+    public class ThreadResult
+    {
+        private Stack<MethodResult> threadMethods;
+        private List<MethodResult> tracedMethods;
+
+        [DataMember(Name = "id")]
+        public int ThreadID
+        { get; internal set; }
+
+        public long Time
+        {
+            get
+            {
+                long time = 0;
+                foreach (MethodResult methodResult in tracedMethods)
+                {
+                    time += methodResult.Time;
+                }
+                return time;
+            }
+        }
+
+        [DataMember(Name = "time")]
+        public string TimeWithPostfix
+        {
+            get => Time.ToString() + "ms";
+            private set { } // to allow serialization
+        }
+
+        [DataMember(Name = "methods")]
+        public List<MethodResult> InnerMethods
+        {
+            get => new List<MethodResult>(tracedMethods);
+            private set { } // to allow serialization
+        }
+
+        protected void AddThreadMethod(MethodResult methodResult)
+        {
+            if (threadMethods.Count > 0)
+            {
+                threadMethods.Peek().AddInnerMethod(methodResult);
+            }
+            else
+            {
+                tracedMethods.Add(methodResult);
+            }
+            threadMethods.Push(methodResult);
+        }
+
+        internal void StartTracingMethod(MethodResult methodResult)
+        {
+            AddThreadMethod(methodResult);
+            methodResult.StartTrace();
+        }
+
+        internal void StopTracingMethod()
+        {
+            if (threadMethods.Count == 0)
+            {
+                throw new NoMethodTracingException(ThreadID);
+            }
+            threadMethods.Pop().StopTrace();
+        }
+
+        internal ThreadResult(int id)
+        {
+            threadMethods = new Stack<MethodResult>();
+            tracedMethods = new List<MethodResult>();
+            ThreadID = id;
+        }
+    }
+}
