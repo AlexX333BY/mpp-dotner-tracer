bd1860e9b4ffdd7a54f9c696fb938b2c18852f83
Aliaksei <alexx333by@gmail.com>
2018-09-11 23:55:31 +0300

    ConsoleTraceResultWriter implemented

---
+++ Tracer/Tracer/ConsoleTraceResultWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tracer
+{
+    public class ConsoleTraceResultWriter : ITraceResultWriter
+    {
+        public ITraceResultSerializer Serializer
+        { set; protected get; }
+
+        public void Write(TraceResult traceResult)
+        {
+            Serializer.Stream = Console.OpenStandardOutput();
+            Serializer.SerializeTraceResult(traceResult);
+        }
+
+        public ConsoleTraceResultWriter()
+        {
+            Serializer = null;
+        }
+
+        public ConsoleTraceResultWriter(ITraceResultSerializer serializer)
+        {
+            Serializer = serializer;
+        }
+    }
+}
